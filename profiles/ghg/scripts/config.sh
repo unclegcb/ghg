7a406bfaf5e7b15026a61b178da38823bfb7f74e
Gabe Carleton-Barnes <gabe@thinkshout.com>
2013-09-12 20:55:06 -0700

    Baseline drupal build

---
+++ profiles/ghg/scripts/config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# match your install profile name
+PROJECT=ghg
+
+# pantheon, acquia, generic
+HOSTTYPE=pantheon
+
+SITENAME="Gamers Helping Gamers"
+
+GITREPO="ssh://codeserver.dev.e31cc06c-782b-4b2e-a3fb-16585c8f4caa@codeserver.dev.e31cc06c-782b-4b2e-a3fb-16585c8f4caa.drush.in:2222/~/repository.git"
